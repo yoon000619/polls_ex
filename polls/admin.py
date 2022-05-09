613c2699f7fffbe63b53e9e50d52cc3f209035c1
yoon000619 <104990765+yoon000619@users.noreply.github.com>
2022-05-09 15:56:58 +0900

    i

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
 from polls.models import Question, Choice
 
-admin.site.register(Question)
+#class QuestionAdmin(admin.ModelAdmin):
+    #fields = ['pub_date', 'question_text']
+
+#class QuestionAdmin(admin.ModelAdmin):
+    #fieldsets = [('Question Statement', {'fields':['question_text']}),
+                # ('Date Information', {'fields': ['pub_date']})]
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [('Question Statement', {'fields':['question_text']}),
+                 ('Date Information', {'fields': ['pub_date'], 'classes':['collapse']})]
+
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
 
 
